c56870456ddef5dffc352d2302b50f8e412d742f
Wookie Choi <hyunwook2010@gmail.com>
2022-09-16 15:10:29 -0400

    fixed some syntax errors in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var passLeng = 0;
 
 var passArray = [];
 var numArray = ['1','2','3','4','5','6','7','8','9','0'];
-var specArray = ['!','\"','#','$','%','&','*','(',')','\'','+',',','.','-','/','<','>','=','?','@','[',']','^','_','\\','`','{','}','~','|'];
+var specArray = ['!','"','#','$','%','&','*','(',')','+',',','.','-','/','<','>','=','?','@','[',']','^','_','`','{','}','~','|'];
 var lowerArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
@@ -22,7 +22,7 @@ function promptForPass() {
 
   passLeng = parseInt(prompt("How many characters do you want your password to contain?"));
   //if length is within bounds
-  if (passLeng < 8 || passLeng > 128 || isNaN(passLeng)) {
+  if (passLeng < 8 || passLeng > 128) {
     //error message
     alert("Password length must be a number within 8 to 128");
     //stop
@@ -32,7 +32,7 @@ function promptForPass() {
   //if yes
   if (confirm("Click to confirm including numeric characters")) {
     //concat numbers to final array
-    passArray = passArray.concat(lowerArray);
+    passArray = passArray.concat(numArray);
   }
 
   //if yes
@@ -42,13 +42,13 @@ function promptForPass() {
   }
 
   //if yes
-  if (confirm("Click to confirm including lowercase characters")) {
+  if (confirm("Click to confirm including lowercase letters")) {
     //concat lowercase to final array
     passArray = passArray.concat(lowerArray);
   }
 
   //if yes
-  if (confirm("Click to confirm including uppercase characters")) {
+  if (confirm("Click to confirm including uppercase letters")) {
     //concat uppercase to final array
     passArray = passArray.concat(upperArray);
   }
@@ -58,16 +58,16 @@ function promptForPass() {
 
 //make random pass
 function generatePassword() {
-  //console.log("You clicked the button");
 
   //empty password
   var passReturn = "";
 
   //loop scramble password
   for(var i = 0; i < passLeng; i++) {
-    var scrambleIndex = Math.floor(Math.random() * passArray.length);
+    var scrambleIndex = Math.random() * passArray.length;
     //place array elements into password
-    password = password + passArray[scrambleIndex];
+    passReturn = passReturn + passArray[scrambleIndex];
+    console.log(scrambleIndex);
   }
   //return value
   return passReturn;
